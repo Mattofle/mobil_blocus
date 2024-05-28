7bbf94a880986b22a8261b1aa5458feab6222c0a
Erismann <matteo.erismann@student.vinci.be>
2024-05-28 10:41:16 +0200

    F09.2 Affichage des films

--- ex5/lib/views/film_row.dart
+++ ex5/lib/views/film_row.dart
@@ -13,16 +13,41 @@ class FilmRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(film.title),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("Directed by ${film.director} - ${film.running_time} minutes"),
-          Text("Release date: ${film.release_date}"),
-          Text("Description: ${film.description}"),
-          Text("Rotten Tomatoes score: ${film.rt_score}"),
-        ],
+    return Card(
+      margin: EdgeInsets.all(8),
+      child: Padding(
+        padding: EdgeInsets.all(8),
+        child: Row(
+          children: [
+            Image.network(film.image, width: 100),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(film.release_date),
+                  const Divider(),
+                  Text(film.title, style: Theme.of(context).textTheme.headline6),
+                  const Divider(),
+                  Text("${film.rt_score}%",
+                        style: TextStyle(
+                          color: int.parse(film.rt_score) > 75
+                              ? Colors.green
+                              : int.parse(film.rt_score) > 50
+                              ? Colors.orange
+                              : Colors.red,
+                        ),
+                  ),
+                  const Divider(),
+                  Text("Directed by ${film.director} - ${film.running_time} minutes"),
+                  const Divider(),
+                  Text(film.description,textAlign: TextAlign.justify,
+                    softWrap: true,),
+                ],
+              ),
+            )
+          ],
+        )
       ),
     );
   }
